f87b2fd6cad3635bc93591dbca1737a842155bc5
Romain-Dailly <loops51@hotmail.com>
2019-04-19 01:13:17 +0200

    deleted unuseful.js, selecting characters possible

--- index.js
+++ index.js
@@ -18,16 +18,28 @@ console.log(characters)
 const listOfCharacters = document.getElementById("showCharacters")
 for (let i = 0; i < 6; i++) {
   listOfCharacters.innerHTML += 
-  `<a hover="cursor:pointer">
+  `<a class="character-card" onclick="setCharacter('${characters[i].image}','${characters[i].name}')">
     <div class="card mr-2" style="width: 7rem;">
       <img class="card-img-top img-fluid"src="${characters[i].image}">     
       <div class="card-body">
       <p class="card-text">${characters[i].name}</p>
       </div>
-  </div></a>`;
+    </div>
+  </a>`;
   };
 }
-
+// function to set selected character
+const setCharacter = (image, name)=>{
+  characterName = name
+  characterImage = image
+  document.getElementById('character').innerHTML = `
+    <div class="card m-2 col-1.5" "style="width: 7rem">
+      <img class="card-img-top img-fluid"src="${characterImage}">     
+      <div class="card-body">
+        <p class="card-text">${characterName}</p>
+      </div>
+    </div>`  
+}
 // store pseudo
 let pseudo = "";
 function getPseudo() {
@@ -45,7 +57,7 @@ function GetRandomCharacter() {
     characterName = response.name;
     characterImage = response.image;
     document.getElementById('character').innerHTML = `
-    <div class="card mr-2" style="width: 10rem;height: 13rem;">
+    <div class="card m-2" >
       <img class="card-img-top img-fluid"src="${characterImage}">     
       <div class="card-body">
         <p class="card-text">${characterName}</p>
