528b1abeb27456261aff3386e385b7a9599a1d4c
Maelenn <ms44118@gmail.com>
2019-04-18 15:54:48 +0200

    homepage creation with input for pseudo

---
+++ pseudoChoice.js
@@ -0,0 +1,13 @@
+function pseudoChoice(x){
+  return(
+    <div>
+      <p>x</p>
+    </div>
+  );
+}
+
+console.log(pseudoChoice("welcome"));
+
+// <div>
+//   URL: <input value={this.state.value} onChange={this.handleUrl} />
+// </div>
