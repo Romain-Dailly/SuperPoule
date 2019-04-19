1d820034c1d8075765216fc4d17323bbbb71681a
Maelenn <ms44118@gmail.com>
2019-04-19 11:26:32 +0200

    variables pseudo and image stores from index.HTML to playGame.HTML

--- index.js
+++ index.js
@@ -31,23 +31,29 @@ for (let i = 0; i < 6; i++) {
   </a>`;
   };
 }
+
+// function to transfert variables
+// function transfertVariables (myString) {
+//   window.location.href = "playGame.html" + myString;
+// }
 // function to set selected character
 const setCharacter = (image, name)=>{
   characterName = name
   characterImage = image
+  var queryString = "playGame.html?playerPseudo=" + pseudo + "&playerImage=" + characterImage;
   document.getElementById('character').innerHTML = `
     <div class="card m-2 col-1.5" "style="width: 7rem">
       <img class="card-img-top img-fluid"src="${characterImage}">     
       <div class="card-body">
-        <p class="card-text">${characterName}</p>
-        <a href="./playGame.html"><button>PLAY</button></a>
-      </div>
-    </div>`  
+      <p class="card-text">${characterName}</p>
+      <a href="${queryString}"><button>PLAY</button></a>
+    </div>
+  </div>`  
 }
 // store pseudo
 function getPseudo() {
   pseudo = document.getElementById("myPseudo").value;
-  document.getElementById('pseudo').innerHTML = `ready to play ${pseudo} ?`;     
+  // document.getElementById('pseudo').innerHTML = `ready to play ${pseudo} ?`;     
 }
 // choose a character randomly
 function GetRandomCharacter() {
@@ -56,16 +62,21 @@ function GetRandomCharacter() {
   .then(response => {
     characterName = response.name;
     characterImage = response.image;
+    //variables a transmettre a playGame.HTML
+    var queryString = "?playerPseudo=" + pseudo + "&playerImage=" + characterImage;
+    window.location.href = "playGame.html" + queryString;
+    //affichage dans index.HTML
     document.getElementById('character').innerHTML = `
     <div class="card m-2" >
       <img class="card-img-top img-fluid"src="${characterImage}">     
       <div class="card-body">
-        <p class="card-text">${characterName}</p>
-        <p class="card-text">${pseudo}</p>
-        <a href="./playGame.html"><button>PLAY</button></a>
+      <p class="card-text">${pseudo}</p>
+      <p>alias</p>
+      <p class="card-text">${characterName}</p>
+        <a href="${window.location.href}"><button>PLAY</button></a>
       </div>    
-    </div>
-    `  
+    </div>`
+
   })     
 }
 
